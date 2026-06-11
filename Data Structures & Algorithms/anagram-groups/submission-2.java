3740cc6c127d2732b61d61f01898349fa32710aa
VISHAL SINGH RANA <115576294+ranavishal0002@users.noreply.github.com>
2026-06-12 02:25:39 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] str) {
+          
+        int n =str.length;
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(int i =0; i<n ; i++){
+            char ch[] = str[i].toCharArray();
+            Arrays.sort(ch);
+
+            String sorted = new String(ch);
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<String>());
+            }
+            map.get(sorted).add(str[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+}/***
+
+  int n = str.length;
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(int i =0; i<n; i++){
+            char ch[] = str[i].toCharArray();
+            Arrays.sort(ch);
+
+            String sorted = new String(ch);
+
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<String>());
+            }
+            map.get(sorted).add(str[i]);
+        }
+        return new ArrayList<>(map.values());
+**/
