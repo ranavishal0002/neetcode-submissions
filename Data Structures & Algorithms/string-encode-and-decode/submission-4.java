c5a883adc6a447755d9ea80befa868d130c98c72
VISHAL SINGH RANA <115576294+ranavishal0002@users.noreply.github.com>
2026-07-05 01:14:33 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String encode(List<String> strs) {
+      StringBuilder encode = new StringBuilder();
+     for(String str: strs)
+     {
+       encode.append(str.length())
+       .append("#")
+       .append(str);
+     }
+     return encode.toString();
+    }
+    public List<String> decode(String str) {
+      ArrayList<String> list = new ArrayList<>(); 
+      int i= 0;
+      while(i< str.length())
+      {
+        int j =i;
+
+        while(str.charAt(j)!='#'){
+          j++;
+        }
+        int length = Integer.parseInt(str.substring(i,j));
+        j++;
+        String word = str.substring(j, j+length);
+        list.add(word);
+        i = j+length;
+      }
+      return list;
+    }
+} 
