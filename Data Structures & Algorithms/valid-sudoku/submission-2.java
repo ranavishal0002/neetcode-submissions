ee1c37791ec23def5db84410d71e6a59dd6632f6
VISHAL SINGH RANA <115576294+ranavishal0002@users.noreply.github.com>
2026-07-14 23:06:35 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[]rows = new HashSet[9];
+        HashSet<Character>[]cols = new HashSet[9];
+        HashSet<Character>[]boxes = new HashSet[9];
+
+        //initialise Hashset
+        for(int i=0; i<9; i++){
+            rows[i]= new HashSet<>();
+            cols[i]= new HashSet<>();
+            boxes[i]= new HashSet<>();
+        }
+
+        //traverse the grid/board 
+        for(int row=0; row<9; row++){
+            for(int col=0; col<9; col++){
+
+                char curr = board[row][col];
+
+                // as we don't need to validate empty cell
+                if(curr == '.'){
+                    continue;
+                }
+                int box = (row/3)*3 +(col/3);
+
+                if(rows[row].contains(curr) || cols[col].contains(curr) || boxes[box].contains(curr)){
+                    return false;
+                }
+                // else we need to add the no. in the set respectively
+                rows[row].add(curr);
+                cols[col].add(curr);
+                boxes[box].add(curr);
+            }
+        }
+        return true;
+    }
+}
